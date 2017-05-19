419d0ebe9772939f1cac75ca696725aa64701dcf
Denis <Nolane16@gmail.com>
2017-05-19 23:46:32 +0300

    Move window creation in method

--- WorkWithDatabase/src/workwithdatabase/MainController.java
+++ WorkWithDatabase/src/workwithdatabase/MainController.java
@@ -2,6 +2,7 @@ package workwithdatabase;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -13,34 +14,34 @@ public class MainController {
     public void setStage(Stage stage) {
         this.stage = stage;
     }
+    
+    private <T> void runDialog(String title, Parent root) throws IOException {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.initOwner(stage);
+        window.setTitle(title);
+        window.setResizable(false);
+        window.setScene(new Scene(root));
+        window.showAndWait();
+    }
 
     @FXML
     private void onMakeDelivery() throws IOException {
-        Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(stage);
-        dialog.setResizable(false);
-        dialog.setScene(new Scene(new DeliveryController()));
-        dialog.showAndWait();
+        runDialog("Доставка", new DeliveryController());
     }
 
     @FXML
     private void onMakeTransfer() throws IOException {
-        Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(stage);
-        dialog.setResizable(false);
-        dialog.setScene(new Scene(new TransferController()));
-        dialog.showAndWait();
+        runDialog("Транспортировка", new TransferController());
     }
 
     @FXML
     private void onMakeSupply() throws IOException {
-        Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(stage);
-        dialog.setResizable(false);
-        dialog.setScene(new Scene(new SupplyController()));
-        dialog.showAndWait();
+        runDialog("Поставка", new SupplyController());
+    }
+    
+    @FXML
+    private void onGoodsTable() throws IOException {
+        runDialog("Справочник товаров", new GoodsTableController());
     }
 }
